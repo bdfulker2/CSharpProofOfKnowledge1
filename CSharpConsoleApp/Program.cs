8c28315589aa2c7b106a47e5867a70109b1d5285
benjmain <bdfulker2@hotmail.com>
2020-09-03 13:31:16 -0400

    Final commit for first console application.

--- CSharpConsoleApp/Program.cs
+++ CSharpConsoleApp/Program.cs
@@ -6,7 +6,16 @@ namespace CSharpConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to my C# Console Application");
+           
+            Console.WriteLine("Press Enter to continue");
+
+            /****
+            * Introduces the .ReadLine to pause application. This is done to 
+            * allow user to actually see the output as it would just print output
+            * and user would likely not be able to read it due to speed.
+            */
+            Console.ReadLine();
         }
     }
 }
