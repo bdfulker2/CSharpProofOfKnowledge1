23ac4fd0cf73384fe7b89ed6ca2de710cf393a3b
benjmain <bdfulker2@hotmail.com>
2020-09-10 13:50:23 -0400

    Added Virtual SendMessage method to Person.cs

--- SchoolLibrary/Person.cs
+++ SchoolLibrary/Person.cs
@@ -14,5 +14,14 @@ namespace SchoolLibrary
         public string LastName { get; set; }
         public string Email { get; set; }
         public abstract float ComputeGradeAverage();
+        public virtual string SendMessage(string message)
+        {
+            var sb = new StringBuilder();
+            var timeStamp = string.Format("Sent on {0:D} at {0:t}", DateTime.Now);
+            sb.AppendLine(timeStamp);
+            sb.AppendLine("Dear " + FirstName + ",");
+            sb.AppendLine(message);
+            return sb.ToString();
+        }
     }
 }
