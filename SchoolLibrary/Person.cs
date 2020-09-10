bcfd4fba3467bf3470641e6bda42ab03cd4ced56
benjmain <bdfulker2@hotmail.com>
2020-09-10 12:49:20 -0400

    Added Teacher.cs class with inheritence from abstract Person.cs class

--- SchoolLibrary/Person.cs
+++ SchoolLibrary/Person.cs
@@ -9,6 +9,5 @@ namespace SchoolLibrary
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Subject { get; set; }
     }
 }
