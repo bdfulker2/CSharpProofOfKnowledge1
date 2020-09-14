1a5703955e8c38dff583d0d76048b20740749d04
benjmain <bdfulker2@hotmail.com>
2020-09-14 09:05:31 -0400

    Added properties to EnglishPaper.cs

--- SchoolLibrary/EnglishPaper.cs
+++ SchoolLibrary/EnglishPaper.cs
@@ -6,6 +6,8 @@ namespace SchoolLibrary
 {
     public class EnglishPaper
     {
-
+        public string Title { get; set; }
+        public int MinimumWordCount { get; set; }
+        public string PaperText { get; set; }
     }
 }
