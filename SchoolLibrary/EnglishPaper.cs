63a66ffebff578df4011bb9bad34cbab1774fe07
benjmain <bdfulker2@hotmail.com>
2020-09-14 09:11:30 -0400

    Added properties to EnglinshPaper.cs and created IScored.cs interface with properties

--- SchoolLibrary/EnglishPaper.cs
+++ SchoolLibrary/EnglishPaper.cs
@@ -4,10 +4,24 @@ using System.Text;
 
 namespace SchoolLibrary
 {
+    /// <summary>
+    /// Class for assignment type English Paper
+    /// </summary>
     public class EnglishPaper
     {
+        /// <summary>
+        /// Property for accessing Title
+        /// </summary>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Property for accessing Minimum Word count
+        /// </summary>
         public int MinimumWordCount { get; set; }
+
+        /// <summary>
+        /// Property for accessing the text of the paper
+        /// </summary>
         public string PaperText { get; set; }
     }
 }
