3a4cfd9e9994715e9dec5873787a9fa3a6898fb7
benjmain <bdfulker2@hotmail.com>
2020-09-15 14:27:12 -0400

    initial commit of SimpleArrayTests.cs

---
+++ EssentialTraining2Tests/SimpleArrayTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining2;
+
+namespace EssentialTraining2Tests
+{
+    [TestClass]
+    public class SimpleArrayTests
+    {
+        [TestMethod]
+        public void SimpleArrayTest()
+        {
+
+        }
+    }
+}
