cd1079e6d7950ba43c49d4be59e49aaa190bc85b
benjmain <bdfulker2@hotmail.com>
2020-09-15 16:17:33 -0400

    Added Test logic for SimpleArraryTests.cs

--- EssentialTraining2Tests/SimpleArrayTests.cs
+++ EssentialTraining2Tests/SimpleArrayTests.cs
@@ -9,10 +9,20 @@ namespace EssentialTraining2Tests
     [TestClass]
     public class SimpleArrayTests
     {
+        
+       
         [TestMethod]
         public void SimpleArrayTest()
         {
-
+            var testSimpleArrayInstance = new SimpleArray();
+            Assert.AreEqual(4, testSimpleArrayInstance.GroceryList.Length);
+            Assert.AreEqual("Milk", testSimpleArrayInstance.GroceryList[1].ToString());
+        }
+        [TestMethod]
+        public void TestToString()
+        {
+            var testSimpleArrayInstance = new SimpleArray();
+            Assert.IsTrue(testSimpleArrayInstance.ToString().StartsWith("There are"));
         }
     }
 }
