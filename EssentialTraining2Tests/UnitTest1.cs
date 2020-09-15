fb65f71bfeb3f64439663fff9598a49183201d5a
benjmain <bdfulker2@hotmail.com>
2020-09-15 11:33:45 -0400

    Added unit test

--- EssentialTraining2Tests/UnitTest1.cs
+++ EssentialTraining2Tests/UnitTest1.cs
@@ -9,6 +9,9 @@ namespace EssentialTraining2Tests
         [TestMethod]
         public void TestMethod1()
         {
+            var testInstnace = new Class1();
+            var testResult = testInstnace.AddTwo(9, 5);
+            Assert.AreEqual(14, testResult, "I expect 9 + 5 = 14");
         }
     }
 }
