5155a450965f251904637aa1ada992a01a8b8584
benjmain <bdfulker2@hotmail.com>
2020-09-15 13:47:03 -0400

    Created SimpleArray.cs

---
+++ EssentialTraining2/SimpleArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EssentialTraining2
+{
+    public class SimpleArray
+    {
+        public string[] GroceryList;
+
+        public SimpleArray()
+        {
+            GroceryList = new string[4] { "Bread", "Milk", "Eggs", "Cheese" };
+        }
+
+        public override string ToString()
+        {
+            for(int i = 0; i < GroceryList.Length; i++)
+            {
+                
+            }
+            return "There are " + GroceryList.Length + "and they are " + GroceryList.ToString();
+        }
+    }
+}
