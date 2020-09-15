5e7c4fe148e53194cd5e0867abdfc2d6cd589be9
benjmain <bdfulker2@hotmail.com>
2020-09-15 16:21:17 -0400

    Initial Commit of SimpleList.cs

---
+++ EssentialTraining2/SimpleList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining2
+{
+    public class SimpleList
+    {
+        public List<string> Sauces { get; set; }
+        public SimpleList()
+        {
+            Sauces = new List<string>();
+        }
+        public bool IsSauceAwesome(string sauce)
+        {
+            return Sauces.Contains(sauce);
+        }
+    }
+}
