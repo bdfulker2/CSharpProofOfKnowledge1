7b86a80e6d795e5cf019cd99b94e567b47891995
benjmain <bdfulker2@hotmail.com>
2020-09-17 07:47:26 -0400

    Fixed formatting and removed excess spacing

--- EssentialTraining2Tests/SimpleArrayTests.cs
+++ EssentialTraining2Tests/SimpleArrayTests.cs
@@ -9,20 +9,18 @@ namespace EssentialTraining2Tests
     [TestClass]
     public class SimpleArrayTests
     {
-        
-       
         [TestMethod]
         public void SimpleArrayTest()
         {
             var testSimpleArrayInstance = new SimpleArray();
             Assert.AreEqual(4, testSimpleArrayInstance.GroceryList.Length);
-            Assert.AreEqual("Milk", testSimpleArrayInstance.GroceryList[1].ToString());
+            Assert.AreEqual("Milk", testSimpleArrayInstance.GroceryList[1]);
         }
         [TestMethod]
         public void TestToString()
         {
             var testSimpleArrayInstance = new SimpleArray();
-            Assert.AreEqual("There are 4 and they are Bread, Milk, Eggs, Cheese.", testSimpleArrayInstance.ToString(),);
+            Assert.AreEqual("There are 4 and they are Bread, Milk, Eggs, Cheese.", testSimpleArrayInstance.ToString());
         }
     }
 }
