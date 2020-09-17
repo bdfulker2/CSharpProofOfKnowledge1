39b325b583a4ede3d3af7a880fe2a093981ec77e
benjmain <bdfulker2@hotmail.com>
2020-09-17 08:05:04 -0400

    FlowControl.cs initial commit

---
+++ EssentialTraining2/FlowControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining2
+{
+    public class FlowControl
+    {
+        public bool IsYourFavoriteColorBlue(string color)
+        {
+            /***
+             * Returns true if two strings match and false if they don't.
+             */
+            return string.Equals(color, "blue", StringComparison.CurrentCultureIgnoreCase);
+            
+            /***
+             * Examples of how flow control method above can be written.
+             * if(string.Equals(color, "blue", StringComparison.CurrentCultureIgnoreCase))
+            * {
+            *     return true;
+            * }else
+            * {
+            *     return false;
+            * }
+            *
+            * if(color.ToLower() == "blue")
+            * {
+            *     return true;
+            * }else
+            * {
+            *     return false;
+            * }
+            */
+
+        }
+    }
+}
