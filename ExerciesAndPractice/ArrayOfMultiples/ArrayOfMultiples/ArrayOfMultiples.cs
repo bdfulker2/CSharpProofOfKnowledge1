753af76f96f93318424750e3770d4f8e8c19bcdb
benjmain <bdfulker2@hotmail.com>
2020-09-27 06:56:06 -0400

    Added in function ArrayOfMultiples

--- ExerciesAndPractice/ArrayOfMultiples/ArrayOfMultiples/ArrayOfMultiples.cs
+++ ExerciesAndPractice/ArrayOfMultiples/ArrayOfMultiples/ArrayOfMultiples.cs
@@ -2,7 +2,14 @@
 
 namespace ArrayOfMultiples
 {
+    /// <summary>
+    /// Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num up to length.
+    /// </summary>
     public class ArrayOfMultiples
     {
+        public int[] ArrayOfMulitples(int num, int length)
+        {
+            return new int[] { };
+        }
     }
 }
