cc31ff245e7ff9ec24fa0f0f492360c3562b1550
benjmain <bdfulker2@hotmail.com>
2020-09-28 16:18:41 -0400

    Added optimized search method.

--- ExerciesAndPractice/FindingTargetValueInSortedArray/FindingTargetVAlueInSortedArrayTests/FindingTargetValueTests.cs
+++ ExerciesAndPractice/FindingTargetValueInSortedArray/FindingTargetVAlueInSortedArrayTests/FindingTargetValueTests.cs
@@ -23,7 +23,7 @@ namespace FindingTargetVAlueInSortedArrayTests
         [TestCase(10, new int[] { 10, 11, 12, 14, 15, 16, 17, 18, 19 }, ExpectedResult = true)]
         [TestCase(10, new int[] { 1, 3, 4, 5, 6, 7, 8, 9, 10 }, ExpectedResult = true)]
         [TestCase(10, new int[] { 11, 12, 14, 15, 16, 17, 18, 19 }, ExpectedResult = false)]
-        public bool OptimizedFindTargetValueInArrayTest(int target, int[] arrayOfValues)
+         public bool OptimizedFindTargetValueInArrayTest(int target, int[] arrayOfValues)
         {
             FindingTargetValue targetValue = new FindingTargetValue();
             return targetValue.OptimizedFindTargetValueInArray(target, arrayOfValues);
--- ExerciesAndPractice/FindingTargetValueInSortedArray/FindingTargetValueInSortedArray/FindingTargetValue.cs
+++ ExerciesAndPractice/FindingTargetValueInSortedArray/FindingTargetValueInSortedArray/FindingTargetValue.cs
@@ -13,9 +13,30 @@ namespace FindingTargetValueInSortedArray
         /// <returns></returns>
         public bool FindTargetValueInArray(int target, int[] arrayOfValues)
         {
-            for(int i = 0; i < arrayOfValues.Length; i++)
+            for (int i = 0; i < arrayOfValues.Length; i++)
             {
                 if (arrayOfValues[i] == target) return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Optimized by iterating in both directions until value is found.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="arrayOfValues"></param>
+        /// <returns></returns>
+        public bool OptimizedFindTargetValueInArray(int target, int[] arrayOfValues)
+        {
+            int i = 0;
+            int j = arrayOfValues.Length -1;
+            while (i < arrayOfValues.Length)
+            {
+                if (arrayOfValues[i] == target) return true;
+                else i++;
+                if (arrayOfValues[j] == target) return true;
+                else j--;
+                if (i == j) return false;
             }
             return false;
         }
