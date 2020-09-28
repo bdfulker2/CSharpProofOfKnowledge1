d62e5418f268005c8022ff4269dd20ce35ddd9de
benjmain <bdfulker2@hotmail.com>
2020-09-28 14:55:52 -0400

    Added new OptimizedFindTargetValueInArrayTest

--- ExerciesAndPractice/FindingTargetValueInSortedArray/FindingTargetVAlueInSortedArrayTests/FindingTargetValueTests.cs
+++ ExerciesAndPractice/FindingTargetValueInSortedArray/FindingTargetVAlueInSortedArrayTests/FindingTargetValueTests.cs
@@ -17,5 +17,16 @@ namespace FindingTargetVAlueInSortedArrayTests
             FindingTargetValue targetValue = new FindingTargetValue();
             return targetValue.FindTargetValueInArray(target, arrayOfValues);
         }
+
+        [Test]
+        [TestCase(10, new int[] { 1, 3, 4, 5, 6, 8, 10, 11, 12, 14 }, ExpectedResult = true)]
+        [TestCase(10, new int[] { 10, 11, 12, 14, 15, 16, 17, 18, 19 }, ExpectedResult = true)]
+        [TestCase(10, new int[] { 1, 3, 4, 5, 6, 7, 8, 9, 10 }, ExpectedResult = true)]
+        [TestCase(10, new int[] { 11, 12, 14, 15, 16, 17, 18, 19 }, ExpectedResult = false)]
+        public bool OptimizedFindTargetValueInArrayTest(int target, int[] arrayOfValues)
+        {
+            FindingTargetValue targetValue = new FindingTargetValue();
+            return targetValue.OptimizedFindTargetValueInArray(target, arrayOfValues);
+        }
     }
 }
