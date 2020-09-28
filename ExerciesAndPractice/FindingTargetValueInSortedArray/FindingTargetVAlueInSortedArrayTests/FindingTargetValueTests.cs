e4832d6ea24072cfe2cc8936c6bbcec07b96ac69
benjmain <bdfulker2@hotmail.com>
2020-09-28 14:53:28 -0400

    Added Test cases for see speed of edge cases

--- ExerciesAndPractice/FindingTargetValueInSortedArray/FindingTargetVAlueInSortedArrayTests/FindingTargetValueTests.cs
+++ ExerciesAndPractice/FindingTargetValueInSortedArray/FindingTargetVAlueInSortedArrayTests/FindingTargetValueTests.cs
@@ -8,7 +8,10 @@ namespace FindingTargetVAlueInSortedArrayTests
     public class FindingTargetValueTests
     {
         [Test]
-        [TestCase(10, new int[] {1, 3, 4, 5, 6, 8, 10, 11, 12, 14 },  ExpectedResult = true)]
+        [TestCase(10, new int[] { 1, 3, 4, 5, 6, 8, 10, 11, 12, 14 }, ExpectedResult = true)]
+        [TestCase(10, new int[] { 10, 11, 12, 14, 15, 16, 17, 18, 19 }, ExpectedResult = true)]
+        [TestCase(10, new int[] { 1, 3, 4, 5, 6, 7, 8, 9, 10}, ExpectedResult = true)]
+        [TestCase(10, new int[] { 11, 12, 14, 15, 16, 17, 18, 19 }, ExpectedResult = false)]
         public bool FindTargetValueInArrayTest(int target, int[] arrayOfValues)
         {
             FindingTargetValue targetValue = new FindingTargetValue();
