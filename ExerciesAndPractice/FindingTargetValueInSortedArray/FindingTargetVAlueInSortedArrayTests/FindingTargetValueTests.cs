87c568f2b4744967db5c110cbe68115fd9635bb9
benjmain <bdfulker2@hotmail.com>
2020-09-30 10:03:54 -0400

    Created test for Optimized find target sum of two integers in an array

--- ExerciesAndPractice/FindingTargetValueInSortedArray/FindingTargetVAlueInSortedArrayTests/FindingTargetValueTests.cs
+++ ExerciesAndPractice/FindingTargetValueInSortedArray/FindingTargetVAlueInSortedArrayTests/FindingTargetValueTests.cs
@@ -39,5 +39,16 @@ namespace FindingTargetVAlueInSortedArrayTests
             FindingTargetValue targetValue = new FindingTargetValue();
             return targetValue.FindTargetSumInArray(target, arrayOfValues);
         }
+
+        [Test]
+        [TestCase(10, new int[] { 1, 3, 4, 5, 6, 8, 10, 11, 12, 14 }, ExpectedResult = true)]
+        [TestCase(10, new int[] { 1, 5, 6, 7, 8, 9, 10, 11, 12, 14 }, ExpectedResult = true)]
+        [TestCase(10, new int[] { 1, 3, 4, 5, 6, 7, 8, 9, 10 }, ExpectedResult = true)]
+        [TestCase(10, new int[] { 11, 12, 14, 15, 16, 17, 18, 19 }, ExpectedResult = false)]
+        public bool OptimizedFindTargetSumInArrayTest(int target, int[] arrayOfValues)
+        {
+            FindingTargetValue targetValue = new FindingTargetValue();
+            return targetValue.FindTargetSumInArray(target, arrayOfValues);
+        }
     }
 }
