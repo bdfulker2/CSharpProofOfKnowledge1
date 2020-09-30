0f5dd2225c4740ed4079c820681e27efb0b266b6
benjmain <bdfulker2@hotmail.com>
2020-09-30 09:04:08 -0400

    Added test for Finding target sum of two values in an array

--- ExerciesAndPractice/FindingTargetValueInSortedArray/FindingTargetVAlueInSortedArrayTests/FindingTargetValueTests.cs
+++ ExerciesAndPractice/FindingTargetValueInSortedArray/FindingTargetVAlueInSortedArrayTests/FindingTargetValueTests.cs
@@ -23,10 +23,21 @@ namespace FindingTargetVAlueInSortedArrayTests
         [TestCase(10, new int[] { 10, 11, 12, 14, 15, 16, 17, 18, 19 }, ExpectedResult = true)]
         [TestCase(10, new int[] { 1, 3, 4, 5, 6, 7, 8, 9, 10 }, ExpectedResult = true)]
         [TestCase(10, new int[] { 11, 12, 14, 15, 16, 17, 18, 19 }, ExpectedResult = false)]
-         public bool OptimizedFindTargetValueInArrayTest(int target, int[] arrayOfValues)
+        public bool OptimizedFindTargetValueInArrayTest(int target, int[] arrayOfValues)
         {
             FindingTargetValue targetValue = new FindingTargetValue();
             return targetValue.OptimizedFindTargetValueInArray(target, arrayOfValues);
+        }
+
+        [Test]
+        [TestCase(10, new int[] { 1, 3, 4, 5, 6, 8, 10, 11, 12, 14 }, ExpectedResult = true)]
+        [TestCase(10, new int[] { 1, 5, 6, 7, 8, 9, 10, 11, 12, 14 }, ExpectedResult = true)]
+        [TestCase(10, new int[] { 1, 3, 4, 5, 6, 7, 8, 9, 10 }, ExpectedResult = true)]
+        [TestCase(10, new int[] { 11, 12, 14, 15, 16, 17, 18, 19 }, ExpectedResult = false)]
+        public bool FindTargetSumInArrayTest(int target, int[] arrayOfValues)
+        {
+            FindingTargetValue targetValue = new FindingTargetValue();
+            return targetValue.FindTargetSumInArray(target, arrayOfValues);
         }
     }
 }
