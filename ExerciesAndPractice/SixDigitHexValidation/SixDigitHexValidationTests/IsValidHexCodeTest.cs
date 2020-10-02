12f87541dc3d6d31ae3f1bcfe57ea00207000256
benjmain <bdfulker2@hotmail.com>
2020-10-02 10:52:24 -0400

    Added test attritbutes and logic

--- ExerciesAndPractice/SixDigitHexValidation/SixDigitHexValidationTests/IsValidHexCodeTest.cs
+++ ExerciesAndPractice/SixDigitHexValidation/SixDigitHexValidationTests/IsValidHexCodeTest.cs
@@ -1,12 +1,18 @@
 using System;
+using NUnit.Framework;
+using SixDigitHexValidation;
 
 namespace SixDigitHexValidationTests
 {
+    [TestFixture]
     public class UnitTest1
     {
-        public void IsValidHexCodeTest()
+        [Test]
+        public void IsValidHexCodeTest(string hexCode)
         {
-
+            IsValidHexCode sixDigHexValidation = new IsValidHexCode();
+            Console.WriteLine($"Input : {hexCode}");
+            return sixDigHexValidation.IsValidHexCode(hexCode);
         }
     }
 }
