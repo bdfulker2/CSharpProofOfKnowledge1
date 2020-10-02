80eee21a546bbc1a39f1f82071f85087f2cdb5b6
benjmain <bdfulker2@hotmail.com>
2020-10-02 11:02:24 -0400

    Add method for ValidateSixDigitHexCode to IsValidHexCode.cs

--- ExerciesAndPractice/SixDigitHexValidation/SixDigitHexValidation/IsValidHexCode.cs
+++ ExerciesAndPractice/SixDigitHexValidation/SixDigitHexValidation/IsValidHexCode.cs
@@ -2,7 +2,18 @@
 
 namespace SixDigitHexValidation
 {
+    /// <summary>
+    /// Exercies is to:
+    /// Create a function that determines whether a string is a valid hex code.
+    /// A hex code must begin with a pound key # and is exactly 6 characters in length.
+    /// Each character must be a digit from 0-9 or an alphabetic character from A-F. 
+    /// All alphabetic characters may be uppercase or lowercase.
+    /// </summary>
     public class IsValidHexCode
     {
+        public bool ValidateSixDigitHexCode(string hexValue)
+        {
+            return false;
+        }
     }
 }
