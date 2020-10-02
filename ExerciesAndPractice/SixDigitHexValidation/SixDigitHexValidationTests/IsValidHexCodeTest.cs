887a9a29507ea7b157fa627688748e7c992ebe23
benjmain <bdfulker2@hotmail.com>
2020-10-02 10:58:38 -0400

    Added a sufficient number of test cases

--- ExerciesAndPractice/SixDigitHexValidation/SixDigitHexValidationTests/IsValidHexCodeTest.cs
+++ ExerciesAndPractice/SixDigitHexValidation/SixDigitHexValidationTests/IsValidHexCodeTest.cs
@@ -8,6 +8,21 @@ namespace SixDigitHexValidationTests
     public class UnitTest1
     {
         [Test]
+        [TestCase("#CD5C5C", ExpectedResult = true)]
+        [TestCase("#EAECEE", ExpectedResult = true)]
+        [TestCase("#eaecee", ExpectedResult = true)]
+        [TestCase("#CD5C58C", ExpectedResult = false, Description = "Length exceeds 6.")]
+        [TestCase("#CD5C5Z", ExpectedResult = false, Description = "Alphabetic characters not in A-F.")]
+        [TestCase("#CD5C&C", ExpectedResult = false, Description = "Unacceptable character.")]
+        [TestCase("CD5C5C", ExpectedResult = false, Description = "Does not start with #.")]
+        [TestCase("#123CCCD", ExpectedResult = false, Description = "Length exceeds 6.")]
+        [TestCase("#123456", ExpectedResult = true)]
+        [TestCase("#987654", ExpectedResult = true)]
+        [TestCase("#9876543", ExpectedResult = false, Description = "Length exceeds 6.")]
+        [TestCase("#CCCCCC", ExpectedResult = true)]
+        [TestCase("#ZCCZCC", ExpectedResult = false, Description = "Not acceptable alphabetic characters.")]
+        [TestCase("#Z88Z99", ExpectedResult = false, Description = "Not acceptable alphabetic characters.")]
+        [TestCase("#Z88!99", ExpectedResult = false, Description = "Unacceptable character.")]
         public void IsValidHexCodeTest(string hexCode)
         {
             IsValidHexCode sixDigHexValidation = new IsValidHexCode();
