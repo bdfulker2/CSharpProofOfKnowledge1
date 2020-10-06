b9a5f28b69abfbd1574cf14bb17dda6a22a2cb83
benjmain <bdfulker2@hotmail.com>
2020-10-06 14:55:28 -0400

    Added testcases

--- ExerciesAndPractice/HurdleJumper/HurdleJumperTests/HurdleJumperTests.cs
+++ ExerciesAndPractice/HurdleJumper/HurdleJumperTests/HurdleJumperTests.cs
@@ -8,6 +8,11 @@ namespace HurdleJumpersTests
     public class HurdleJumperTests
     {
         [Test]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 5, ExpectedResult = true)]
+        [TestCase(new int[] { 5, 5, 3, 4, 5 }, 3, ExpectedResult = false)]
+        [TestCase(new int[] { 5,4,5,6 }, 10, ExpectedResult = true)]
+        [TestCase(new int[] { 1, 2, 1 }, 1, ExpectedResult = false)]
+        [TestCase(new int[] { }, 1, ExpectedResult = true)]
         public bool HurdleJumpTest(int[] hurdleHeights, int jumpersJumpHeight)
         {
             HurdleJumper hurdle = new HurdleJumper();
